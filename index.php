a9652e0fb3412a9d20d9c0ba66f758ac2225765f
Sp00dlepuss <matejdalekorej@gmail.com>
2018-03-22 15:00:25 +0100

    Využití if a else

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>6. práce</title>
+  </head>
+  <body>
+    <h1>6. práce: podmínky</h1>
+
+    <?php
+    $plnoletost=18;
+
+
+    if ($plnoletost >=18) {
+     echo "на здоровье";
+     ?>
+     <img src="kvas.jpg" alt="alcohol">
+
+     <?php
+   } else { ?>
+    <?php echo "Извините, вам не 15";  ?> <img src="kys.jpg" alt="No_Chlast"> <?php  } ?> 
+
+
+
+
+
+
+
+
+  </body>
+</html>
